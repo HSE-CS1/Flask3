10dc14545d3233a362916d66377e918d8d0ab6fb
Fishel <nfishel@hse.k12.in.us>
2023-02-23 18:16:51 +0000

    updated layout

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
 
@@ -7,7 +8,13 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-
+@app.route("/rand")
+def rand():
+  r = randint(1,5)
+  if r == 3:
+    return f"<h1>{r} NOT FOUND!</h1>", 404
+  else:
+    return f"{r} --> BINGO!!!"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port="80", debug=True)
